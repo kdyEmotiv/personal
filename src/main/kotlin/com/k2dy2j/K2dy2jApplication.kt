3d82c39e268afa66ac7754458a472c02fbd02a7b
kdyEmotiv <113403129+kdyEmotiv@users.noreply.github.com>
2023-10-18 16:47:15 +0900

    기본 세팅

---
+++ src/main/kotlin/com/k2dy2j/K2dy2jApplication.kt
@@ -0,0 +1,11 @@
+package com.k2dy2j
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class K2dy2jApplication
+
+fun main(args: Array<String>) {
+    runApplication<K2dy2jApplication>(*args)
+}
---
+++ src/test/kotlin/com/k2dy2j/K2dy2jApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.k2dy2j
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class K2dy2jApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
